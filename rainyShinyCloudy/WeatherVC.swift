45ab3e0adf6f352fc8bf785636046a4aff71f66c
Lagoiko Nick <lagoiko.nick@gmail.com>
2017-06-02 05:25:12 +0300

    UI added. IBOutlets created

---
+++ rainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherVC.swift
+//  rainyShinyCloudy
+//
+//  Created by Nick on 02.06.17.
+//  Copyright © 2017 Lagoiko Nick. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTemlLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+    
+
+
+
+}
+
